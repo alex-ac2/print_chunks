92c68363e13c9e393aa9508e12e2542777177e81
Alex Curci <alex.curci@gmail.com.com>
2019-06-03 19:45:02 +0000

    Commit of callbacks for step 6

---
+++ callbacks/print_lowercase.js
@@ -0,0 +1,13 @@
+var getHTML = require('./http_functions');
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step6/lowercase.html'
+};
+
+function printUpperCase (html) {
+  console.log(html.toLowerCase());
+}
+
+getHTML(requestOptions, printUpperCase);
+
